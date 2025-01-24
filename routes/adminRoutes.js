28de62466d3a0153ddf0c34d0950603cfefbb6a3
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-01-24 16:10:10 -0500

    Fixing Mistakes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { verifyAdmin, clearCache } from '../controllers/adminController.js';
-import { isAdmin } from '../middleware/auth.js';
+import { authenticateToken, isAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/', verifyAdmin);
 
-router.delete('/clear', isAdmin, clearCache);
+router.delete('/clear', authenticateToken, isAdmin, clearCache);
 
 export default router;
